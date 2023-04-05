cf49e434ae084bde6b0e9493ee1e39a7c15e8eb5
Trishan Wagle <mailtotrishan@gmail.com>
2023-04-05 20:18:29 +0545

    feat: changed loader

--- src/components/Utils/Loader.jsx
+++ src/components/Utils/Loader.jsx
@@ -3,7 +3,7 @@ const Loader = () => {
         <div className="loader">
             <img
                 className="load-spinner"
-                src="loader.gif"
+                src="https://cdn.dribbble.com/users/24447/screenshots/1201310/media/58f89dcf65bada3422fc33dd05752f32.gif"
                 alt="Loading..."
             />
         </div>
