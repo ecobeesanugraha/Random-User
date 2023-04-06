063d5b86b847e16468f8e203a1baec6a83a04539
Trishan Wagle <mailtotrishan@gmail.com>
2023-04-06 10:23:58 +0545

    feat: dark mode feature added.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+
 const Navbar = () => {
+    const [mode, setMode] = useState("Light Mode")
+    const [dark, setDark] = useState(false)
+    const [icon, setIcon] = useState(faMoon)
+    useEffect(() => {
+        let active = true
+        if (active) {
+            dark ? setIcon(faSun) : setIcon(faMoon)
+            mode == "Light Mode" ? setMode("Dark Mode") : setMode("Light Mode")
+            mode == "Light Mode" ? document.body.className = "light-mode" : document.body.className = "dark-mode"
+        }
+
+        return () => {
+            active = false
+        }
+    }, [dark])
     return (
         <nav className="nav">
             <h1 className="nav-heading">Random <span className="nav-heading--span">User</span></h1>
+            <button type="button" onClick={() => { setDark(!dark) }} className="toggle-button" id="toggle-button">
+                {mode}
+                <FontAwesomeIcon icon={icon} className="nav-toggle-mode" />
+            </button>
         </nav>
     );
 }
--- src/components/Utils/Loader.jsx
+++ src/components/Utils/Loader.jsx
@@ -1,9 +1,11 @@
+import loadingSpinner from "../../assets/loader.gif";
+
 const Loader = () => {
     return (
         <div className="loader">
             <img
                 className="load-spinner"
-                src="https://cdn.dribbble.com/users/24447/screenshots/1201310/media/58f89dcf65bada3422fc33dd05752f32.gif"
+                src={loadingSpinner}
                 alt="Loading..."
             />
         </div>
